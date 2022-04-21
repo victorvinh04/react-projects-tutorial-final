3182320eed7edb7072e6ac7904e8b19340da0bb4
thuongvb <baothuong9608@gmail.com>
2022-04-21 14:11:30 +0700

    basic project with context

--- 13-stripe-submenus/setup/src/Submenu.js
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -1,7 +1,49 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from "react";
+import { useGlobalContext } from "./context";
 
 const Submenu = () => {
-  return <h2>submenu component</h2>
-}
+  const {
+    isSubMenuOpen,
+    location,
+    page: { page, links },
+  } = useGlobalContext();
+  const container = useRef(null);
+  const [columns, setColumns] = useState("col-2");
+  useEffect(() => {
+    setColumns("col-2");
+    const submenu = container.current;
+    const { center, bottom } = location;
+    submenu.style.left = `${center}px`;
+    submenu.style.top = `${bottom}px`;
 
-export default Submenu
+    if (links.length === 3) {
+      setColumns("col-3");
+    }
+    if (links.length > 3) {
+      setColumns("col-4");
+    }
+  }, [page, location, links]);
+  return (
+    <aside
+      className={`${isSubMenuOpen ? "submenu show" : "submenu"}`}
+      ref={container}
+    >
+      <section>
+        <h4>{page}</h4>
+        <div className={`submenu-center ${columns}`}>
+          {links.map((link, index) => {
+            const { url, icon, label } = link;
+            return (
+              <a key={index} href={url}>
+                {icon}
+                {label}
+              </a>
+            );
+          })}
+        </div>
+      </section>
+    </aside>
+  );
+};
+
+export default Submenu;
--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,49 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
+  const [location, setLocation] = useState({});
+  const [page, setPage] = useState({page: '', links: []});
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openSubmenu = (text, coordinates) => {
+      const page = sublinks.find(link => link.page === text);
+      setPage(page);
+    setLocation(coordinates);
+    setIsSubMenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubMenuOpen(false);
+  };
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isSubMenuOpen,
+        openSidebar,
+        closeSidebar,
+        openSubmenu,
+        closeSubmenu,
+        location,
+        page,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
