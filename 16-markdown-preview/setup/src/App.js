925c89fedc9f35c770c04427460b9eb5e76c5288
thuongvb <baothuong9608@gmail.com>
2022-04-24 21:12:17 +0700

    markdown and pagination

--- 16-markdown-preview/setup/src/App.js
+++ 16-markdown-preview/setup/src/App.js
@@ -1,8 +1,24 @@
-import React, { useState } from 'react'
-import ReactMarkdown from 'react-markdown'
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
 
 function App() {
-  return <h2>markdown preview starter</h2>
+  const [markdown, setMarkdown] = useState("## markdown preview");
+  return (
+    <main>
+      <section className="markdown">
+        <textarea
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+          className="input"
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+        ></textarea>
+        <ReactMarkdown>{markdown}</ReactMarkdown>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
